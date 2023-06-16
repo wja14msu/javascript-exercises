1ef725532082d93beba0faae15389598871b4c30
Wander5 <wja14msu@gmail.com>
2023-06-16 18:28:59 -0500

    answer reverse string problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+function reverseString(string) {
+    let revArray = [];
+    let revString = '';
+    let stringArray = string.split("");
+    for (i = stringArray.length - 1; i >= 0; i--) {
+        revArray.push(stringArray[i]);
+        revString = revArray.join('');
+    }
+    return revString;
 };
 
 // Do not edit below this line
