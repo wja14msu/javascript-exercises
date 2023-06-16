47be0085b0893ac3ad3c7a902d8c3feb30bd74f7
Wander5 <wja14msu@gmail.com>
2023-06-16 17:26:59 -0500

    answer challenges

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-
+let string = "";
+/*const repeatString = function() {
+    for (i = 0; i < 3; i++) {
+        string += 'hey'
+    }
+    return string;
 };
+*/
+function repeatString(string, num) {
+    if (num === 0) {
+        return string = '';
+    } else if (num < 0) {
+        return string = 'ERROR';
+    } else if (string === '') {
+        return string = '';
+    }
+    for (i = 0; i < num - 1; i++) {
+        string += 'hey';
+    }
+    return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
