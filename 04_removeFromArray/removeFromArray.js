595a214782b69228a28f06ee02bdd5ae99e6cb1d
Wander5 <wja14msu@gmail.com>
2023-06-18 16:10:46 -0500

    answer 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...args) {
+    array = array.filter(x => !args.includes(x))
+    return array;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
