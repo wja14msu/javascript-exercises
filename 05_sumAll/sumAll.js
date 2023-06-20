1c78881454f032bf78e398b5711a2efc4f71252e
Wander5 <wja14msu@gmail.com>
2023-06-20 06:00:30 -0500

    sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
 
-};
 
+function sumAll(firstNumber, secondNumber) {
+  let total = 0;
+  for (i = firstNumber; i <= secondNumber; i++) {
+    total += i;
+  }
+  return total;
+  
+}
+
+
+/*const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+*/  
 // Do not edit below this line
 module.exports = sumAll;
