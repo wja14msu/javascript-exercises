10f7efd0f43e74252d4c65aeff3a0eafe99cdcfd
Wander5 <wja14msu@gmail.com>
2023-06-21 06:41:07 -0500

    answer leapYears problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+    if (year % 4 === 0 && year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 // Do not edit below this line
 module.exports = leapYears;
